01a98584ad54722edc1f6314a64ed41bb47074cd
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-23 11:18:55 +0300

    Finish : Module #4.1. Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        int max = first;
+
+        max = second > max ? second : max;
+
+        max = third > max ? third : max;
+
+        System.out.println(max);
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+
+        String firstOne = "first";
+
+        String secondOne = "second";
+
+        String result = null;
+
+        result = first > second ? firstOne : secondOne;
+
+        System.out.println(result);
+
     }
 }
